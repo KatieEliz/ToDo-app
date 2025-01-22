ae656c1f0e05dac511009779c77e509de962ebe9
katieEliz <katie.morton@bjss.com>
2025-01-22 10:23:28 +0000

    updated struct

--- pkg/todo.go
+++ pkg/todo.go
@@ -5,22 +5,24 @@ import (
 	"os"
 )
 
-type Todo struct {
-	Description string
+type TodoItem struct {
+	ID          int    `json: "id"`
+	Description string `json:"description"`
+	Status      string `json: "status"`
 }
 
-func LoadTodos() ([]Todo, error) {
+func LoadTodos() ([]TodoItem, error) {
 	file, err := os.Open("todos.json") //try to open file, error if doesnt exist
 
 	if err != nil { //if error, checks if exists using isnotexist
 		if os.IsNotExist(err) {
-			return []Todo{}, nil //doesnt exist empty list of todos
+			return []TodoItem{}, nil //doesnt exist empty list of todos
 		}
 		return nil, err
 	}
 	defer file.Close() //function finish = file close
 
-	var todos []Todo
+	var todos []TodoItem
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&todos)
 
