ff8c3a75c269fbe3b1464590532b03dbf6cac059
katieEliz <katie.morton@bjss.com>
2025-01-23 15:32:47 +0000

    refactor main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,44 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"todo-app/pkg"
 )
 
 func main() {
+	add := flag.String("add", "", "Add a new to-do item.")
+	update := flag.String("update", "", "Update a to-do item, format ID:Description.")
+	delete := flag.Int("delete", -1, "Delete a to-do item by ID.")
+	list := flag.Bool("list", false, "List all to-do items.")
+	status := flag.String("status", "", "Update the status of a to-do item, format ID:Status.")
+
+	flag.Parse()
+
 	todos, err := pkg.LoadTodos()
 	if err != nil {
-		log.Fatal("Error loading todos:", err)
+		fmt.Printf("Error loading to-dos: %v\n", err)
+		return
 	}
 
-	fmt.Println("Loaded Todos:")
-	pkg.ListTodos(todos)
-	for _, todo := range todos {
-		fmt.Printf("ID: %d, Description: %s, Status: %s\n", todo.ID, todo.Description, todo.Status)
-	}
+	switch {
+	case *add != "":
+		todos = pkg.AddTodo(todos, *add)
 
-	todos = pkg.AddTodo(todos, "Test new task")
-	err = pkg.SaveTodos(todos)
-	if err != nil {
-		log.Fatal("Error saving todos:", err)
-	}
+	case *update != "":
+		pkg.UpdateTodoDescription(todos, *update)
 
-	fmt.Println("\nTodos after adding a new item:")
-	for _, todo := range todos {
-		fmt.Printf("ID: %d, Description: %s, Status: %s\n", todo.ID, todo.Description, todo.Status)
-	}
+	case *delete != -1:
+		todos = pkg.DeleteTodo(todos, *delete)
 
-	pkg.UpdateTodoDescription(todos, "1:Updated task description")
+	case *list:
+		pkg.ListTodos(todos)
 
-	todos = pkg.DeleteTodo(todos, 1)
-	fmt.Println("\nTodos after deleting item with ID 1:")
-	for _, todo := range todos {
-		fmt.Printf("ID: %d, Description: %s, Status: %s\n", todo.ID, todo.Description, todo.Status)
-	}
+	case *status != "":
+		pkg.UpdateTodoStatus(todos, *status)
 
-	err = pkg.SaveTodos(todos)
-	if err != nil {
-		log.Fatal("Error saving todos after deletion:", err)
+	default:
+		fmt.Println("No operation specified. Use --help for available commands.")
 	}
 }
